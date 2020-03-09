19611df07c2299142f136e5588fa18ac9d2ab21e
yassineuuu <57989575+yassineuuu@users.noreply.github.com>
2020-03-09 15:01:51 +0100

    start-commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+        // indexof/startxith/endwith/split/toLowerCase/toUpperCase
+
+var favoriteBooks=[
+    "Harry Potter", "Game of Thrones","Overlord"
+]
+     
+ function printFavoriteBooks(){
+        for( var y=0;y<favoriteBooks.length; y+=1){
+            var book=favoriteBooks[y]
+            console.log(book) 
+        }
+      }
+
+      function addFavoriteBook(bk){
+        var n=bk.includes("Great")
+          if (n!=true){
+            favoriteBooks.push(bk)
+          }
+          return printFavoriteBooks()
+      }
+
+
+
+     
+
+         
+
+
