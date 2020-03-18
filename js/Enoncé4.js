541cf90dcffdef476a9e39385c8132db8c412ba3
yassineuuu <57989575+yassineuuu@users.noreply.github.com>
2020-03-18 23:51:58 +0100

    Create Enoncé4.js

---
+++ js/Enoncé4.js
@@ -0,0 +1,18 @@
+let card =  5;
+switch (card){
+    case 2 :
+    case 3 :
+    case 4 :
+    case 5 :
+    case 6 :
+        console.log(+1)
+        break;
+    case 7 :
+    case 8 :
+    case 9 :
+        console.log(0)
+        break;
+    default:
+        console.log(-1)
+        break;
+}
