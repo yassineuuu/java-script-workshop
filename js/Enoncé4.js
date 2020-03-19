650b380026fcd7864e761afa5c1fef5d3d6f7e97
yassineuuu <57989575+yassineuuu@users.noreply.github.com>
2020-03-19 21:01:43 +0100

    Enoncé4 ajustement
    i had a misunderstood the objectif
    so here is the function

--- js/Enoncé4.js
+++ js/Enoncé4.js
@@ -1,18 +1,20 @@
-let card =  5;
-switch (card){
-    case 2 :
-    case 3 :
-    case 4 :
-    case 5 :
-    case 6 :
-        console.log(+1)
-        break;
-    case 7 :
-    case 8 :
-    case 9 :
-        console.log(0)
-        break;
-    default:
-        console.log(-1)
-        break;
+function Blackjack(carte){
+    let compt=0
+    switch (carte){
+        case 2 :
+        case 3 :
+        case 4 :
+        case 5 :
+        case 6 :
+            compt=1
+            break;
+        case 7 :
+        case 8 :
+        case 9 :
+            compt=-1
+            break;
+     
+    }
+    return compt
+
 }
